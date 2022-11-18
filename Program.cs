94841e48cb3ec91bf1ba56e0148482b80c13ffc9
FUNman3310 <tu7ldh0l3@code.edu.az>
2022-11-18 05:55:10 +0400

    complited

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = {1,2,3,4,5};
+ChangeArr(arr);
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
+
+
+static void ChangeArr( params int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = 0;
+    }
+}
